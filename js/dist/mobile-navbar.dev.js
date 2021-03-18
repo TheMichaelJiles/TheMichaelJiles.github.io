723c75549a89ba1f43dd7c4493dc1c60428c92b3
Stanley Jiles <sjiles1@my.westga.edu>
2021-03-18 11:02:22 -0400

    mobile

--- js/dist/mobile-navbar.dev.js
+++ js/dist/mobile-navbar.dev.js
@@ -2,9 +2,11 @@
 
 var mobileNav = document.getElementById("mobile-nav");
 var mobileNavButton = document.getElementById("mobile-nav-button");
+var docBody = document.getElementById("doc-body");
 
 function toggleMobileNav() {
   mobileNav.classList.toggle("hide-mobile-nav");
   mobileNav.classList.toggle("mobile-nav");
   mobileNavButton.classList.toggle("open-button");
+  docBody.classList.toggle("disable-scrolling");
 }
